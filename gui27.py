252eeee8c0b990b0f9a8beab958a182019efc15e
ShadmanQuazi <shadmanhquazi@gmail.com>
2017-02-25 22:37:50 -0500

    added some more elements to gui27

--- gui27.py
+++ gui27.py
@@ -1,8 +1,10 @@
 from Tkinter import *
 
-
 root = Tk()
-
-lapel = Label(root, text='Hello world!')
-lapel.pack()
-lapel.mainloop()
+newLabel = Label(root, text="Hello!")
+newLabel.pack(side=LEFT)
+newEntry = Entry(root, bd =5)
+newEntry.pack(side =LEFT)
+newButt = Button(root, text = "Go!")
+newButt.pack(side=RIGHT)
+root.mainloop()
