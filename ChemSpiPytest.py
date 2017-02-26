3a5740d61c90483445151ff6ad72340cc8f6a893
ShadmanQuazi <shadmanhquazi@gmail.com>
2017-02-25 22:31:05 -0500

    added a basic search function to ChemSpiPytest

--- ChemSpiPytest.py
+++ ChemSpiPytest.py
@@ -4,6 +4,13 @@ cs = ChemSpider('53a402f5-f9d1-410b-bd66-8237844f03f8')
 c = cs.get_compound(2157)
 print(c.molecular_formula)
 print(c.common_name)
-print(c.molar_mass)
+print(str(c.molecular_weight) + ' g/mol')
+print(c.spectra)
 #53a402f5-f9d1-410b-bd66-8237844f03f8
 #xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+for result in cs.search('LSD'):
+    print(result.csid)
+    thing = result.csid
+
+d = cs.get_compound(thing)
+print(d.common_name)
