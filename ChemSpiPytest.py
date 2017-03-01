dbaaec303ab9a5d6862eee8e12dd2ed9578258ca
ShadmanQuazi <shadmanhquazi@gmail.com>
2017-03-01 11:55:20 -0500

    added first bit of user functionality to ChemSpiPy test

--- ChemSpiPytest.py
+++ ChemSpiPytest.py
@@ -1,16 +1,27 @@
+#53a402f5-f9d1-410b-bd66-8237844f03f8
 from chemspipy import ChemSpider
 
 cs = ChemSpider('53a402f5-f9d1-410b-bd66-8237844f03f8')
-c = cs.get_compound(2157)
-print(c.molecular_formula)
-print(c.common_name)
-print(str(c.molecular_weight) + ' g/mol')
-print(c.spectra)
-#53a402f5-f9d1-410b-bd66-8237844f03f8
-#xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-for result in cs.search('LSD'):
-    print(result.csid)
-    thing = result.csid
+searchString = str(raw_input("what would you like to search? "))
+x =1
+resultList = []
 
-d = cs.get_compound(thing)
-print(d.common_name)
+for result in cs.search(searchString):
+    print(str(x) + ". " + result.common_name)
+    resultList.append(result)
+    x+=1
+
+if len(resultList) == 1:
+    cd = cs.get_compound(resultList[0].csid)
+    print(cd.common_name)
+    print(cd.molecular_weight)
+    print(cd.molecular_formula)
+
+if len(resultList)> 1:
+    print("Your search returned " + str(len(resultList)) + " results. Please specify which compound you are interested in")
+    num = int(raw_input("Enter a number: "))
+
+    cd = cs.get_compound(resultList[0].csid)
+    print(cd.common_name)
+    print(cd.molecular_weight)
+    print(cd.molecular_formula)
